239ad3ad14a54a021f66660583ae54f8db30a295
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-09 16:48:41 +0000

    commit changes 1148

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post("/insert_user_record", { :controller => "venues", :action => "create" })
   post("/update_users/:user_id", { :controller => "users", :action => "update" })
 
-  get("/venues", { :controller => "venues", :action => "create" })
+  get("/venues", { :controller => "venues", :action => "index" })
   post("/insert_venue_record", { :controller => "venues", :action => "create" })
   get("/venue/:an_id", { :controller => "venue", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
